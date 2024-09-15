346da7321af48cf29cbdb692a9c616a2a13e1dea
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-15 00:06:05 -0300

    wip: experience component

---
+++ src/app/components/experience/index.tsx
@@ -0,0 +1,14 @@
+import { SectionTitle } from "../section-title";
+
+
+export function Experience() {
+  return (
+    <div className="experience">
+    <SectionTitle content="Experience"/>
+    <p>Almost five years working as a Software Developer, in companies such as Hospital Albert Einstein, Zappts and IClubs.IO</p>
+    <div className="experience-time">
+
+    </div>
+  </div> 
+  )
+}
