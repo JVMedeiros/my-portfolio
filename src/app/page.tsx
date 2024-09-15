b559c1f028bbe859ecc364e29090b03e624c16f0
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-15 01:54:32 -0300

    feat: project finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
-import Image from "next/image";
-import { Experience } from "./components/experience";
-import { Header } from "./components/header";
-import { EmailIcon } from "./components/icons/email-icon";
-import { Info } from "./components/information";
-import { SocialButtons } from "./components/social-buttons";
+import Image from 'next/image'
+import { Experience } from './components/experience'
+import { Header } from './components/header'
+import { Info } from './components/information'
+
 import "./styles/home.scss"
+import { EmailIcon } from './components/icons/email-icon'
+import { SocialButtons } from './components/social-buttons'
 
 export default function Home() {
   return (
     <main className="container">
-      <Header />
-      <Experience />
-      <Info />
-
-      <div className="buttons">
-        <SocialButtons />
-        <a className="primary-btn" href="mailto: jv.medeiros.gallina@gmail.com">
-          contact me
-          <EmailIcon />
-        </a>
-      </div>
-
+        <Header/>
+        <Experience/>
+        <Info/>
+        <div className="buttons">
+          <SocialButtons/>
+          <a className="btn-primary" href="mailto:teste@gmail.com">
+            contact me
+            <EmailIcon/>
+          </a>
+        </div>
     </main>
-  );
+  )
 }
