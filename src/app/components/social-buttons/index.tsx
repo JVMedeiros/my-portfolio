fa0cdc49825c8f51ad5151f231eca753888e37ec
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-15 02:20:46 -0300

    fix: open links in a new page

--- src/app/components/social-buttons/index.tsx
+++ src/app/components/social-buttons/index.tsx
@@ -7,13 +7,13 @@ import "./styles.scss"
 export function SocialButtons() {
   return (
     <div className="social">
-      <a href="https://www.instagram.com/jvctrmd">
+      <a href="https://www.instagram.com/jvctrmd" target="_blank">
         <InstaIcon />
       </a>
-      <a href="https://www.linkedin.com/in/jvmedeiros/">
+      <a href="https://www.linkedin.com/in/jvmedeiros/" target="_blank">
         <LinkedInIcon />
       </a>
-      <a href="https://github.com/JVMedeiros">
+      <a href="https://github.com/JVMedeiros" target="_blank">
         <GitHubIcon />
       </a>
     </div>
