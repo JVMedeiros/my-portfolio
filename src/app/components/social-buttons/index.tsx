96976753be7f989fc3ba86ae369fb90d1d3dfc39
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-15 01:07:06 -0300

    feat: social buttons component

---
+++ src/app/components/social-buttons/index.tsx
@@ -0,0 +1,25 @@
+import { GitHubIcon } from "../icons/github-icon";
+import { InstaIcon } from "../icons/instagram-icon";
+import { LinkedInIcon } from "../icons/linkedin-icon";
+import { TwitterIcon } from "../icons/twitter-icon";
+
+import "./styles.scss"
+
+export function SocialButtons() {
+  return (
+    <div className="social">
+      <a href="https://www.instagram.com/jvctrmd">
+        <InstaIcon />
+      </a>
+      <a href="https://www.linkedin.com/in/jvmedeiros/">
+        <LinkedInIcon />
+      </a>
+      <a href="https://github.com/JVMedeiros">
+        <GitHubIcon />
+      </a>
+      <a href="https://twitter.com/JVMedeiros">
+        <TwitterIcon />
+      </a>
+    </div>
+  )
+}
