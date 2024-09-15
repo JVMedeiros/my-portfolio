f4b1a9fa9cea478c3158128ec2a8722fca206b6c
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-15 01:56:26 -0300

    fix: code cleanup

--- src/app/components/social-buttons/index.tsx
+++ src/app/components/social-buttons/index.tsx
@@ -1,7 +1,6 @@
 import { GitHubIcon } from "../icons/github-icon";
 import { InstaIcon } from "../icons/instagram-icon";
 import { LinkedInIcon } from "../icons/linkedin-icon";
-import { TwitterIcon } from "../icons/twitter-icon";
 
 import "./styles.scss"
 
