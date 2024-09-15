a50dce1f93f51a7da752e3e32928d37b3327e603
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-15 00:06:35 -0300

    fix: header structure

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 import './styles.scss'
 
 export function Header() {
-  return(
+  return (
     <div className="header">
-    <Image
-      src="/me.jpeg"
-      alt="Next.js logo"
-      width={325}
-      height={310}
-      priority
-    />
-    <div>
-      <h1>Hi, I'm João 👋</h1>
-      <h2>Fullstack Software Engineer</h2>
+      <div>
+        <h1>Hi, I'm João 👋</h1>
+        <h2>Fullstack Software Engineer</h2>
+      </div>
+      <Image
+        src="/me.jpeg"
+        alt="Next.js logo"
+        width={310}
+        height={290}
+        priority
+      />
     </div>
-  </div>
   )
 }
