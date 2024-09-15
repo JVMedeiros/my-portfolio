b51750ed54e867a3595a85215592bbb3c0da36b7
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-14 23:08:54 -0300

    wip: header component

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export function Header() {
+  return(
+    <div className="header">
+    <Image
+      src="https://nextjs.org/icons/next.svg"
+      alt="Next.js logo"
+      width={180}
+      height={38}
+      priority
+    />
+    <div>
+      <h1>Hi, I'm João 👋</h1>
+      <h2>Fullstack Software Engineer</h2>
+    </div>
+  </div>
+  )
+}
